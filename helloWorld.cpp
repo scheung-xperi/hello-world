a1c75ae94e81ea506d392df51923f1fe575d73a6
Simon Cheung <simon.cheung@xperi.com>
2020-03-04 09:56:59 +0000

    Add hello world app and cmake file

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+//
+//  helloWorld.cpp
+//  
+//
+//  Created by Simon Cheung on 04/03/2020.
+//
+
+#include<iostream>
+ 
+int main(int argc, char *argv[]){
+   std::cout << "Hello World!" << std::endl;
+   return 0;
+}
